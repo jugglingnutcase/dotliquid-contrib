400c537b53911f377689f25b7866d6ce6020d640
James R Sconfitto <james@exele.com>
2014-06-27 11:31:14 -0400

    Add mustache block
    Renders anything resulting from within the block as a Mustache template,
    using data within the context from a provided key.

---
+++ src/DotLiquid.Contrib.Mustache.Tests/MustacheBlockTests.cs
@@ -0,0 +1,30 @@
+﻿using DotLiquid;
+using DotLiquid.Contrib.Mustache; using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotLiquid.Contrib.Mustache.Tests
+{
+    public class MustacheBlockTests
+    {
+        [Fact]
+        public void Mustache_block_renders_template()
+        {
+            // Given
+            Template.RegisterTag<MustacheBlock>("mustache");
+            Template template = Template.Parse("{% mustache 'data' %} {% raw %} Hi {{ name }}! {% endraw %} {% endmustache %}");
+
+            Hash data = new Hash();
+            data.Add(new KeyValuePair<string, object>("data", new { name = "Mustache" }));
+
+            // When
+            var result = template.Render(data);
+
+            // Then
+            Assert.Equal("Hi Mustache!".Trim(), result.Trim());
+        }
+    }
+}
---
+++ src/DotLiquid.Contrib.Mustache/MustacheBlock.cs
@@ -0,0 +1,57 @@
+﻿namespace DotLiquid.Contrib.Mustache
+{
+    using DotLiquid;
+    using N = Nustache.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /// <summary>
+    /// Renders a mustache template enclosed by the block using the supplied parameter as a reference the data used in rendering the template.
+    /// </summary>
+    /// <remarks>
+    /// Make sure to pass the key of the data object that the mustache template should render as markup. For example, if you want to send the template
+    /// data from the <code>Person</code> object, call the mustache block like <code>{% mustache "Person" %}</code>. Quotes are optional.
+    /// The block accepts double or single quotes, or no quotes at all.
+    /// </remarks>
+    public class MustacheBlock : Block
+    {
+        private string _modelContext = "";
+
+        public override void Initialize(string tagName, string markup, List<string> tokens)
+        {
+            base.Initialize(tagName, markup, tokens);
+
+            // Ignore any quotes or whitespace in the markup
+            _modelContext = markup.Replace(@"""", @"").Replace(@"'", "").Trim();
+        }
+
+        public override void Render(Context context, TextWriter result)
+        {
+            using (TextWriter tempWriter = new StringWriter())
+            {
+                RenderAll(NodeList, context, tempWriter);
+                try
+                {
+                    // Get the appropriate data to use to render the mustache template
+                    var model = context[_modelContext];
+                    if (model != null)
+                    {
+                        //' Render the mustache template
+                        string mustachioed = N.Render.StringToString(tempWriter.ToString(), model);
+                        result.Write(mustachioed);
+                    }
+                    else
+                    {
+                        result.Write(tempWriter.ToString());
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    result.Write("Rendering Mustache template failed: " + ex.Message);
+                }
+            }
+        }
+    }
+}
