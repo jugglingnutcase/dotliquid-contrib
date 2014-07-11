2da2a2befe04f805a4bc231f6582790cdb2cf820
James R Sconfitto <james@exele.com>
2014-07-11 11:10:52 -0400

    Add includeraw tag

---
+++ src/DotLiquid.Contrib.Mustache.Tests/IncludeRawTagTetsts.cs
@@ -0,0 +1,42 @@
+﻿using DotLiquid.Contrib.Mustache;
+using DotLiquid.FileSystems;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DotLiquid.Contrib.MustacheBlock.Tests
+{
+    public class IncludeRawTagTetsts
+    {
+        [Fact]
+        public void IncludeRaw_tag_returns_only_the_template()
+        {
+            Template.FileSystem = new TestFileSystem("TEMPLATE", "THE TEMPLATE");
+            Template.RegisterTag<IncludeRawTag>("includeraw");
+            Template template = Template.Parse("{% includeraw TEMPLATE %}");
+
+            var result = template.Render();
+
+            Assert.Contains("THE TEMPLATE", result);
+        }
+    }
+
+    public class TestFileSystem : IFileSystem
+    {
+        string templateName;
+        string template;
+        public TestFileSystem(string templateName, string template)
+        {
+            this.templateName = templateName;
+            this.template = template;
+        }
+
+        public string ReadTemplateFile(Context context, string templateName)
+        {
+            if (this.templateName == templateName)
+            {
+                return this.template;
+            }
+            return "FAIL!";
+        }
+    }
+}
---
+++ src/DotLiquid.Contrib.Mustache/IncludeRawTag.cs
@@ -0,0 +1,30 @@
+﻿namespace DotLiquid.Contrib.Mustache
+{
+    using DotLiquid;
+    using DotLiquid.FileSystems;
+    using N = Nustache.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /// <summary>
+    /// Includes a template, but does not Render it through liquid.
+    /// </summary>
+    public class IncludeRawTag : Tag
+    {
+        private string templateName = "";
+
+        public override void Initialize(string tagName, string markup, List<string> tokens)
+        {
+            base.Initialize(tagName, markup, tokens);
+            this.templateName = markup.Trim();
+        }
+
+        public override void Render(Context context, TextWriter result)
+        {
+            // Read the mustache template
+            IFileSystem fileSystem = context.Registers["file_system"] as IFileSystem ?? Template.FileSystem;
+            result.Write(fileSystem.ReadTemplateFile(context, this.templateName));
+        }
+    }
+}
