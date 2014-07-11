60114c94261c08ed8fecd875d25a352318679f2b
James R Sconfitto <james@exele.com>
2014-07-11 10:49:10 -0400

    Add mustache template tag

---
+++ src/DotLiquid.Contrib.Mustache.Tests/MustacheTemplateTagTests.cs
@@ -0,0 +1,71 @@
+﻿using DotLiquid;
+using DotLiquid.FileSystems;
+using DotLiquid.Contrib.Mustache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DotLiquid.Contrib.Mustache.Tests
+{
+    public class TestFileSystem : IFileSystem
+    {
+        string templateName;
+        string template;
+        public TestFileSystem(string templateName, string template)
+        {
+            this.templateName = templateName;
+            this.template = template;
+        }
+
+        public string ReadTemplateFile(Context context, string templateName)
+        {
+            if (this.templateName == templateName)
+            {
+                return this.template;
+            }
+            return "FAIL!";
+        }
+    }
+
+    public class MustacheTemplateTagTests
+    {
+        [Fact]
+        public void Mustache_template_renders_included_mustache_template()
+        {
+            // Given
+            string name = "Artemis";
+            Template.FileSystem = new TestFileSystem("template", "Hi {{ name }}!");
+            Template.RegisterTag<MustacheTemplateTag>("mustachetemplate");
+            Template template = Template.Parse("{% mustachetemplate template data %}");
+            Hash data = new Hash();
+            data.Add(new KeyValuePair<string, object>("data", new { name = name }));
+
+            // When
+            var result = template.Render(data);
+
+            // Then
+            Assert.Equal(string.Format("Hi {0}!", name).Trim(), result.Trim());
+        }
+
+        [Fact]
+        public void Mustache_template_renders_error_when_data_context_not_found()
+        {
+            // Given
+            string name = "Artemis";
+            Template.FileSystem = new TestFileSystem("template", "Hi {{ name }}");
+            Template.RegisterTag<MustacheTemplateTag>("mustachetemplate");
+            Template template = Template.Parse("{% mustachetemplate template ARROWS %}");
+            Hash data = new Hash();
+            data.Add(new KeyValuePair<string, object>("data", new { name = name }));
+
+            // When
+            var result = template.Render(data);
+
+            // Then
+            Assert.Contains("Liquid mustache template issue", result);
+        }
+    }
+}
---
+++ src/DotLiquid.Contrib.Mustache/MustacheTemplateTag.cs
@@ -0,0 +1,60 @@
+﻿namespace DotLiquid.Contrib.Mustache
+{
+    using DotLiquid;
+    using DotLiquid.FileSystems;
+    using N = Nustache.Core;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /// <summary>
+    /// Renders mustache from a template using the passed data context
+    /// </summary>
+    /// <example>
+    /// Used like: <c>{% mustachetemplate template_location data_context %}</c>
+    ///
+    /// The markup after the tag should have two things:
+    /// 1. A mustache template location
+    /// 2. The model context defining which data should render the mustache template
+    /// </example>
+    public class MustacheTemplateTag : Tag
+    {
+        private string modelContext = "";
+        private string templateName = "";
+
+        public override void Initialize(string tagName, string markup, List<string> tokens)
+        {
+            base.Initialize(tagName, markup, tokens);
+
+            // There should be two things:
+            // 1. A mustache template location
+            // 2. The model context saying which data should render the mustache template
+            string[] splitMarkup = markup.Split(new char[] { ' ' });
+            this.templateName = splitMarkup[0];
+            this.modelContext = splitMarkup[1];
+        }
+
+        public override void Render(Context context, TextWriter result)
+        {
+            // Read the mustache template
+            IFileSystem fileSystem = context.Registers["file_system"] as IFileSystem ?? Template.FileSystem;
+            string mustacheTemplate = fileSystem.ReadTemplateFile(context, this.templateName);
+
+            // Get the appropriate data to use to render the mustache template
+            var model = context[this.modelContext];
+
+            // Render the template or an error message
+            string output;
+            if (model != null)
+            {
+                output = N.Render.StringToString(mustacheTemplate, model);
+            }
+            else
+            {
+                output = "Liquid mustache template issue: Not sure which data to render";
+            }
+
+            result.Write(output);
+        }
+    }
+}
